ffc33dded02ceffe755d61a55834a87c11dd35e4
王世隆 <547463736@qq.com>
2018-07-24 18:05:18 +0800

    U

---
+++ libs/utils.js
@@ -0,0 +1,19 @@
+export const getUnixTimeInfo = unixTimeStamp => {
+  if (typeof timeStamp !== 'string')
+    throw new Error('timeStamp must be number type')
+  const curDate = new Date(v * 1000)
+  const year = curDate.getFullYear()
+  const month = `0${curDate.getMonth() + 1}`.slice(-2)
+  const date = `0${curDate.getDate()}`.slice(-2)
+  const hours = `0${curDate.getHours()}`.slice(-2)
+  const mins = `0${curDate.getMinutes()}`.slice(-2)
+  const seconds = `0${curDate.getSeconds()}`.slice(-2)
+  return {
+    year,
+    month,
+    date,
+    hours,
+    mins,
+    seconds
+  }
+}
