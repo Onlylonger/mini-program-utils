007a07260e83d6c382fd33b6aa2999518d5f50ba
王世隆 <547463736@qq.com>
2018-07-26 21:46:56 +0800

    增加月份帮助函数

--- libs/utils.js
+++ libs/utils.js
@@ -1,3 +1,9 @@
+/**
+ * 获取unix时间戳的 各个时间段
+ * @param {number | string} unixTimeStamp 时间戳,单位秒
+ * @return {object}
+ */
+
 export const getUnixTimeInfo = unixTimeStamp => {
   const curDate = new Date(unixTimeStamp * 1000)
   const year = curDate.getFullYear()
@@ -14,4 +20,55 @@ export const getUnixTimeInfo = unixTimeStamp => {
     mins,
     seconds
   }
+}
+
+/**
+ * 产生自增整形数组
+ * @param {number} begin
+ * @param {number} end
+ * @param {number} addNum
+ * @return {number[]}
+ */
+export const getOrderNumberAry = (begin, end, addNum = 1) => {
+  const ary = []
+  for (let i = begin; i <= end; i = i + addNum) {
+    ary.push(i)
+  }
+  return ary
+}
+
+/**
+ *获取某年某月 具体天数
+ * @param {number} year
+ * @param {number} month
+ * @return {number[]}
+ */
+export const getMonthDay = (year, month) => {
+  // 判断是否闰年
+  const isLeapYear = year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+  let array
+
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      array = getOrderNumberAry(1, 31)
+      break
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      array = getOrderNumberAry(1, 30)
+      break
+    case 2:
+      array = isLeapYear ? getOrderNumberAry(1, 29) : getOrderNumberAry(1, 28)
+      break
+    default:
+      throw new Error('month is invalid')
+  }
+  return array
 }
